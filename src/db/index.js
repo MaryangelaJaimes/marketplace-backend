428209ad50a3920d4dbaaa1548badaf63dd0d48d
MaryangelaJaimes <113484670+MaryangelaJaimes@users.noreply.github.com>
2025-02-25 13:30:54 -0300

    cambios db

--- src/db/index.js
+++ src/db/index.js
@@ -7,6 +7,9 @@ const pool = new Pool({
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false, // Esta opción permite conexiones sin validación del certificado, útil si no tienes uno propio
+  },
 });
 
 pool
